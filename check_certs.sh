836d737b7cad076ef1bcbb045723cc83bd2ae972
Gerhard <gerhard@tinned-software.net>
2019-11-13 12:16:47 +0100

    initial version of check_certs

---
+++ check_certs.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#
+# BSD 2-Clause License
+# 
+# Copyright (c) 2019, cs@brnfck.at and gerhard@tinne-software.net
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+if [ $# -lt 1 ]; then
+  echo "ERROR: at least one argument (destination) required" 
+  exit 1
+fi
+
+CCURL=$1
+
+shift
+if [ ! -z "$*" ]; then
+  SSLOPTS=$*
+else
+  SSLOPTS="-subject -issuer -email -dates -fingerprint -noout"
+fi
+
+FILETMP=$(mktemp)
+
+echo | openssl s_client -connect ${CCURL} -showcerts | \
+    awk -v SSLOPTS="${SSLOPTS}" -v FILETMP="${FILETMP}" '
+        /-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/ \
+            {
+                print >>FILETMP
+            }
+        /-----END CERTIFICATE-----/ \
+            { 
+                printf("\nCertificate details\n===================\n")
+                system("cat "FILETMP" | openssl x509 "SSLOPTS" ; echo >"FILETMP)
+            }
+        END \
+            { 
+                print "----------------------------" ;
+                system("rm "FILETMP)
+            }'
